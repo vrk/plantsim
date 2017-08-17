d0804bb5ebff844ec4e7f89ac99ff09292075a94
vrk <vrkwork@gmail.com>
2017-08-17 16:30:11 -0400

    draw ground

--- js/world.js
+++ js/world.js
@@ -5,10 +5,18 @@ class World {
   }
 
   draw() {
-    for (let row = 0; row < PIXELS_WIDE; row++) {
-      for (let col = 0; col < PIXELS_WIDE; col += 2) {
-        const offset = row % 2;
-        this.canvasGrid.update(row, col + offset, 'white');
+    this.drawGround();
+  }
+
+  drawGround() {
+    // Top of soil
+    for (let col = 0; col < PIXELS_WIDE; col++) {
+      this.canvasGrid.update(col, PIXELS_WIDE - GROUND_HEIGHT, '#5F574F');
+    }
+    // Ground
+    for (let row = PIXELS_WIDE - GROUND_HEIGHT + 1; row < PIXELS_WIDE; row++) {
+      for (let col = 0; col < PIXELS_WIDE; col++) {
+        this.canvasGrid.update(col, row, '#AB5236');
       }
     }
     this.canvasGrid.draw();
