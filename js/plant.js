05735f0b14e05c5e7dcb230b11211e8ab6d76695
vrk <vrkwork@gmail.com>
2017-08-17 17:25:39 -0400

    random upward growth

--- js/plant.js
+++ js/plant.js
@@ -15,7 +15,20 @@ class Plant {
     if (this.frontierRow >= 0 && this.seedRow != this.frontierRow) {
       this.canvasGrid.update(this.frontierCol, this.frontierRow, DARK_GREEN);
     }
-    this.frontierRow--;
+
+    // Grow straight until we've broken ground.
+    if (this.frontierRow >= PIXELS_WIDE -  GROUND_HEIGHT) {
+      this.frontierRow--;
+    } else {
+      // Grow in a random direction -- weighted toward up.
+      const random = Math.random();
+      if (random < 0.2) {
+        this.frontierCol = Math.min(PIXELS_WIDE - 1, this.frontierCol + 1);
+      } else if (random < 0.4) {
+        this.frontierCol = Math.max(0, this.frontierCol - 1);
+      }
+      this.frontierRow--;
+    }
     this.canvasGrid.update(this.frontierCol, this.frontierRow, GREEN);
   }
 }
